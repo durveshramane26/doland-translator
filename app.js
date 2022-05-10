f135aef83d6a23ee57af63d5d4e2386c8cd2fde6
durveshramane26 <durveshramane6@gmail.com>
2022-05-10 15:20:21 +0530

    added api

---
+++ app.js
@@ -0,0 +1,22 @@
+const txtinput = document.querySelector('.txt-input');
+const translatebtn = document.querySelector('.translate-btn');
+console.log(translatebtn)
+const txtoutput = document.querySelector('.txt-output');
+console.log(txtoutput);
+
+
+const serverurl = "https://api.funtranslations.com/translate/dolan.json";
+
+function generateURL(text) {
+    return serverurl + "?" + "text=" + text;
+}
+
+function clickHandler() {
+    if(txtinput.value === undefined || txtinput.value === "") {
+        window.alert('Empty input!! Please enter text to get its translation');
+    }
+    fetch(generateURL(txtinput.value))
+    .then(response => response.json())
+    .then(json => {txtoutput.innerText = json.contents.translated});
+}
+translatebtn.addEventListener('click', clickHandler)
